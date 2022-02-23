622a13f6cff927a637f5004b8b8cc8c7bdbfd12f
RuthAja <ruthajalah1@gmail.com>
2022-02-23 13:49:58 +0700

    laravel-admin laravel 9

---
+++ app/Admin/Controllers/ArticleController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Article;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class ArticleController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Article';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Article());
+
+        $grid->column('id', __('Id'));
+        $grid->column('title', __('Name')); // bisa diganti dengan nama yang di dalam kurung dan ini bisa dilakukan untuk yang di bawah
+        $grid->column('content', __('Content'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Article::findOrFail($id));
+
+        $show->column('id', __('Id'));
+        $show->column('title', __('Name'));
+        $show->column('content', __('Content'));
+        $show->column('created_at', __('Created at'));
+        $show->column('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Article());
+
+        $form->text('title', __('Name'));
+        $form->text('content', __('Content'));
+
+        return $form;
+    }
+}
